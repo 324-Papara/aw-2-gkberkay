f92c7e9db17ba3621b8c5a1d11cab20456e41ddf
gkberkay <ay.gkberk@gmail.com>
2024-07-15 14:16:42 +0300

    Deleted Project

--- Para.Base/Entity/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace Para.Base.Entity;
-
-public class BaseEntity
-{
-    public long Id { get; set; }
-    public string InsertUser { get; set; }
-    public DateTime InsertDate { get; set; }
-    public bool IsActive { get; set; }
-}
